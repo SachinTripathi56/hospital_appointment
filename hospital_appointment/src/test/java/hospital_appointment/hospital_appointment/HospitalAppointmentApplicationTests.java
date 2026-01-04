9489656fbcd5e38868057de8eb4dcb3971944422
SachinTripathi56 <agentsachin007@gmail.com>
2026-01-04 21:21:50 +0530

    Pilot

---
+++ hospital_appointment/src/test/java/hospital_appointment/hospital_appointment/HospitalAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital_appointment.hospital_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
